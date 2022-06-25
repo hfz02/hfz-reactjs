657bcacf99f5ee8b236a3ef1a21a3c82eaed627d
hfz02 <muhammad.h@mail.ugm.ac.id>
2022-06-25 10:48:03 +0700

    test

--- src/App.test.js
+++ src/App.test.js
@@ -8,11 +8,21 @@
 // });
 
 
-import React from 'react';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
 import { render } from '@testing-library/react';
 import App from './App';
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  container.querySelector('.App')
+  expect(container.firstChild).toMatchSnapshot();
 });
