b3107941cdfaf6791055b599ef70caef7a6f2b62
hfz02 <muhammad.h@mail.ugm.ac.id>
2022-06-25 10:57:49 +0700

    test

--- src/portfolio/index.jsx
+++ src/portfolio/index.jsx
@@ -8,6 +8,7 @@ import Footer from "./Footer";
 const Portfolio = () => {
     return ( 
         <div>
+            <div><h2>Ini h2</h2></div>
             <Navbar />
             <Biografi />
             <Skills />
