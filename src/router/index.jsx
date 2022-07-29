79a0d4cdfbae24aea2578077b9ff18ec5abce2a4
hfz02 <muhammad.h@mail.ugm.ac.id>
2022-07-29 22:58:26 +0700

    update router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,7 +3,7 @@ import Nav from "./Nav"
 import Home from './Home'
 import About from "./About"
 import CounterApp from "./CounterApp"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 const Routerr = () => {
     return ( 
